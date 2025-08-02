2493c5f667eeb929dde2edca19c1452906e8cced
Sabstsk <raj8271518385@gmail.com>
2025-08-03 03:02:52 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
